ddeaa480a879831046a610732f98bd4037748817
Marco Mata <patrickstar1281@yahoo.com>
2022-06-12 23:10:55 -0500

    got fetch call to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,10 @@ var indexEl = document.querySelector("#index");
 //function to call api
 var apiFunction = function() {
 
-    fetch("http://history.openweathermap.org/data/3.0/history/locations/create?" + indexEl.value + "&appid=98802acd30e720990862db94022d3f8d")
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + inputEl.value + "&limit=5&appid=98802acd30e720990862db94022d3f8d")
     .then(response => response.json).then(data => console.log(data)).catch(err => alert("Enter Valid City"));
 
 };
 
 
-buttonEl.addEventListener("click", apiFunction());
+buttonEl.addEventListener("click", apiFunction);
