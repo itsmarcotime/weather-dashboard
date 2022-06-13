1ea5a83837578e272a66de01b6adb402bb63d6b0
Marco Mata <patrickstar1281@yahoo.com>
2022-06-12 22:48:51 -0500

    read me & save progress

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weather-dashboard
+
+## I am turning in what I have so far. This assignment is still in progress do not what to be late. grade accordantly.
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,10 @@ var indexEl = document.querySelector("#index");
 //function to call api
 var apiFunction = function() {
 
-    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + inputEl.value + "id=524901&appid=98802acd30e720990862db94022d3f8d");
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + inputEl.value + "id=524901&appid=98802acd30e720990862db94022d3f8d")
+    .then(response => response.json).then(data => console.log(data)).catch(err => alert("Enter Valid City"));
 
 };
 
 
-buttonEl.addEventListener("click", )
+buttonEl.addEventListener("click", apiFunction());
